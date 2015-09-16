0304ac3027fe0e3ff59945c45665c32d6f6d94b5
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-09-15 19:24:18 -0500

    file structure. initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jrdevsalarysharer::Application.config.secret_token = '0ebca0b8fcc86523c7303ddaa695d1ffdc856036eb833481159758551350b1c8dba0869c86ae04b24f0de41b2959368dde040783b2b928ea3a8d2c9eaf774cc1'
